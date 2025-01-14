bda5c46e39df7675cb22db79f66b54b6899c0770
EliasIesBelen <eliasureagarcia05@gmail.com>
2025-01-14 18:25:14 +0100

    Conectar HTML y JavaScript

---
+++ modulo 2/01-Fundamentos/assets/js/index.js
@@ -0,0 +1,28 @@
+//alert('hola desde app.js');
+
+//console.log("hola mundo");
+let a = 10, 
+    b = 20, 
+    c = 'Hola ', 
+    d = 'Spiderman',
+    x = a+b;
+
+const saludo = c + d;
+//Mostrar variables
+console.log('a',a);
+console.log('b',a);
+console.log('c',c);
+
+//Mostrar objetos
+console.log({a});
+
+//Mostrar texto añadiendo css
+console.log('%c Mis Variables', 'color:blue');
+
+//Para definir muchas variables creando una tabla
+console.table({a,b,c,d,x})
+c = 'hola de nuevo'
+
+let outerWidth = 1000000;
+const outerHeight = 600;
+
