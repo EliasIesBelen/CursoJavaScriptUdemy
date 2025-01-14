5f74a9a2605de3dcf45472f72f346f9a8fad46dd
EliasIesBelen <eliasureagarcia05@gmail.com>
2025-01-14 18:44:53 +0100

    14/01

---
+++ modulo 2/01-Fundamentos/assets/js/primitivos.js
@@ -0,0 +1,27 @@
+let nombre = 'Peter Parker';
+console.log(nombre);
+
+nombre = 'Ben Parker';
+console.log(nombre);
+
+nombre = "Tia May";
+nombre = `Tia May`;
+
+console.log(typeof nombre);
+nombre = 123;
+console.log(typeof nombre);
+
+ //Se debe poner en minuscula
+let esMarvel = true;
+
+let edad = 33.0002;
+console.log(typeof edad);
+
+
+let superPoder;
+console.log(typeof superPoder);
+
+
+let symboll1 = Symbol('a');
+let symboll2 = Symbol('a');
+console.log(typeof symboll1 === symboll2);
---
+++ modulo 2/Descripcion/utilidad.md
@@ -0,0 +1,1 @@
+En este modulo del curso
